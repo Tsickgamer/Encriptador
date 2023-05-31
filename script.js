b1c321930988e69a52e521b2a987114542755356
Tsickgamer <103135944+Tsickgamer@users.noreply.github.com>
2023-05-31 13:41:05 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function encriptar(){
+    
+    //toLowerCase cambia el texto a minusculas
+    var frase = document.getElementById("textoEncriptado").value.toLowerCase();
+
+    // i = es para que tome letras mayusculas y minusculas
+    // m = es para que tome en cuenta multiples lineas
+    // g = es para que come en cuenta toda la linea u oracion
+
+    var textoEncriptado = frase.replace(/e/img, "enter");
+    var textoEncriptado = textoEncriptado.replace(/o/img, "ober");
+    var textoEncriptado = textoEncriptado.replace(/i/img, "imes");
+    var textoEncriptado = textoEncriptado.replace(/a/img, "ai");
+    var textoEncriptado = textoEncriptado.replace(/u/img, "ufat");
+
+    document.getElementById("textoDesencriptado").innerHTML = textoEncriptado;
+    document.getElementById("botonCopiar").style.display = "show";
+    document.getElementById("botonCopiar").style.display = "inherit";
+}
+function Desencriptar(){
+    
+    var frase = document.getElementById("textoEncriptado").value.toLowerCase();
+
+    var textoEncriptado = frase.replace(/enter/img, "e");
+    var textoEncriptado = textoEncriptado.replace(/ober/img, "o");
+    var textoEncriptado = textoEncriptado.replace(/imes/img, "i");
+    var textoEncriptado = textoEncriptado.replace(/ai/img, "a");
+    var textoEncriptado = textoEncriptado.replace(/ufat/img, "u");
+
+    document.getElementById("textoDesencriptado").innerHTML = textoEncriptado;
+}
+function copiar(){
+
+    var contenido = document.querySelector("#textoDesencriptado");
+        contenido.select();
+        document.execCommand("copy");
+}
